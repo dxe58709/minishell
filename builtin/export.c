516285a5601b63e5f0c66a29816662461d73c912
Nao <N@sakanouenao-no-MacBook-Air.local>
2023-12-20 10:37:15 +0900

    export

--- builtin/export.c
+++ builtin/export.c
@@ -3,12 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: N <nsakanou@student.42tokyo.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:53:09 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/12/12 20:27:19 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/12/19 23:40:35 by N                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+int export_command(char **argv)
+{
+    if (argv[1])
+        return (set_env);
+    else
+        return ();
+    return (EXIT_SUCCESS);
+}
