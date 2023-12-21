020a75da1770b1deab154960e0a1d166e8cca6a2
Nao Sakanoue <nsakanou@c1r19s7.42tokyo.jp>
2023-12-21 22:36:27 +0900

    list

--- builtin/env.c
+++ builtin/env.c
@@ -6,7 +6,7 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 17:52:59 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/12/21 21:17:57 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/12/21 22:35:54 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,41 @@ t_env	*env_search(t_env *env, char *name)
 	return (NULL);
 }
 
+static t_env	*lstlast(t_env *lst)
+{
+	int	i;
+
+	i = 0;
+		printf("ok1\n");
+	while (lst && lst->next)
+	{
+		printf("%p\n", lst->next);
+		i++;
+		lst = lst->next;
+	}
+	return (lst);
+}
+
 void	env_list_add(t_env **env_list, char *name, char *value)
 {
 	t_env	*new_env;
+	t_env	*tmp;
 
 	new_env = (t_env *)malloc(sizeof(t_env));
-	if (!new_env)
+	if (!env_list || !new_env)
 		ft_error();
 	new_env->name = ft_strdup(name);//strdupでexitするか、ここでするか
 	new_env->value = ft_strdup(value);
-	new_env->next = *env_list;
-	*env_list = new_env;
+	new_env->next = NULL;
+	printf("loli\n");
+	if (!*env_list)
+		*env_list = new_env;
+	else
+	{
+		printf("kami\n");
+		tmp = lstlast(*env_list);
+		tmp->next = new_env;
+	}
 }
 
 //与えられた環境変数リスト全体を出力
@@ -62,7 +86,7 @@ t_env	*env_create(char **envp)
 	int		i;
 	t_env	*env_list;
 
-	env_list = (t_env *)malloc(sizeof(t_env));
+	env_list = (t_env *)ft_calloc(1, sizeof(t_env));
 	if (!env_list)
 		ft_error();
 	i = 0;
@@ -71,6 +95,7 @@ t_env	*env_create(char **envp)
 		value = ft_strchr(envp[i], '=') + 1;
 		name = (char *)ft_calloc(value - envp[i] + 1, 1);//calloc失敗した時exit
 		ft_strlcpy(name, envp[i], value - envp[i]);
+		printf("%d:%s=%s,next:%p", i, env_list->name, env_list->value, env_list->next);
 		env_list_add(&env_list, name, value);
 		free(name);
 		name = NULL;
--- builtin/env_utils.c
+++ builtin/env_utils.c
@@ -6,15 +6,15 @@
 /*   By: nsakanou <nsakanou@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 17:42:38 by nsakanou          #+#    #+#             */
-/*   Updated: 2023/12/21 21:20:32 by nsakanou         ###   ########.fr       */
+/*   Updated: 2023/12/21 21:57:43 by nsakanou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-bool	env_name_judge(char *name)//export, unset
+bool env_name_judge(char *name) // export, unset
 {
-	size_t	i;
+	size_t i;
 
 	i = 0;
 	if (!ft_isalpha(name[i]) && name[i] != '_')
@@ -29,18 +29,21 @@ bool	env_name_judge(char *name)//export, unset
 	return (true);
 }
 
-//環境変数の削除
-void	env_del(t_env **env_list, char *name)
+// 環境変数の削除
+void env_del(t_env **env_list, char *name)
 {
-	t_env	*prev;
-	t_env	*current;
+	t_env *prev;
+	t_env *current;
 
 	prev = NULL;
 	current = *env_list;
 	while (current)
 	{
+		printf("%s\n", "ccccc");
+		printf("%s\n", current->name);
 		if (ft_strcmp(current->name, name) == 0)
 		{
+			printf("dddddd\n");
 			if (prev)
 				prev->next = current->next;
 			else
@@ -48,24 +51,27 @@ void	env_del(t_env **env_list, char *name)
 			free(current->name);
 			free(current->value);
 			free(current);
-			return ;
+			return;
 		}
+		printf("%s\n", "aaaaaaa");
 		prev = current;
 		current = current->next;
+		printf("%s\n", "bbbbb");
 	}
+	return;
 }
 
-//環境変数の更新
-//指定された名前の環境変数が存在すればその値を更新し、存在しなければ新しい環境変数として追加
-void	env_update(t_env **env_list, char *name, char *value)
+// 環境変数の更新
+// 指定された名前の環境変数が存在すればその値を更新し、存在しなければ新しい環境変数として追加
+void env_update(t_env **env_list, char *name, char *value)
 {
-	t_env	*current;
+	t_env *current;
 
 	current = env_search(*env_list, name);
 	if (current)
 	{
 		free(current->value);
-		current->value = ft_strdup(value);
+		current->value = ft_strdup(value); // ft_strdupでexit
 	}
 	else
 		env_create((char **)env_list);
